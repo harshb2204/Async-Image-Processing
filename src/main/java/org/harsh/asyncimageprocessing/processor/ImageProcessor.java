c750dd400b0d63f2e063f5f74ab87bba22e1c1bd
Harsh <harshb2204@gmail.com>
2025-11-29 12:14:14 +0530

    image processor

--- src/main/java/org/harsh/asyncimageprocessing/processor/ImageProcessor.java
+++ src/main/java/org/harsh/asyncimageprocessing/processor/ImageProcessor.java
@@ -25,6 +25,16 @@ public class ImageProcessor {
 
      */
 
+    /*
+    ImageProcessor divides a large image into many small tiles and 
+    applies a filter on each tile in parallel using a fixed pool of worker threads.
+    Each tile is processed independently 
+    → results returned as Future<ImageData>
+     → these are later consumed and drawn on a canvas.
+    
+    */
+   
+
     private ExecutorService executorService;
 
     private DrawMultipleImagesOnCanvas drawfn;
@@ -32,7 +42,7 @@ public class ImageProcessor {
     public ImageFilter imageFilter;
 
     public ImageProcessor() {
-        executorService = Executors.newFixedThreadPool(100);
+        executorService = Executors.newFixedThreadPool(100); //create 100 worker threads waiting for tasks.
 
     }
 
@@ -42,6 +52,27 @@ public class ImageProcessor {
 
         List<Future<ImageData>> futures = new ArrayList<>();
 
+        /*
+        A Future can be:
+
+        Not done → result not ready yet.
+
+        Done → result available.
+
+        Failed → throws inside future.get(). 
+        */
+        /*
+         Callable because:
+
+        You need each thread to return processed image data.
+
+        You retrieve results using Future.
+
+        Filtering may throw exceptions.
+
+        The image-processing pipeline depends on task → result flow.
+        
+        */
 
         for (int i = 0; i < numVerticalImages; i++) {
             for (int j = 0; j < numHorizontalImages; j++) {
@@ -49,6 +80,8 @@ public class ImageProcessor {
 
                 int finali = i;
                 int finalj = j;
+                //A Callable in Java is basically a task that you can run in a separate thread 
+                // AND get a return value from.
                 Future<ImageData> future = executorService.submit(new Callable<ImageData>() {
                     @Override
                     public ImageData call() throws Exception {
